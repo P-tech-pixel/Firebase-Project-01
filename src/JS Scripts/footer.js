b2c58b42a3f6f6621b248b7956122e4f669f6573
Prashanta Pandit <prashanta.pandit@secureinn.com.au>
2024-05-14 09:46:17 +1000

    moved index.html on front

--- src/JS Scripts/footer.js
+++ src/JS Scripts/footer.js
@@ -6,7 +6,7 @@ const footer = document.createElement("footer");
 
 // Add some content to the footer
 footer.innerHTML = `
-    <p>&copy; ${currentYear} Your Website Name. All rights reserved.</p>
+    <p>&copy; ${currentYear}  |  magpies.online  |  All rights reserved</p>
 `;
 
 // Append the footer to the body of the document
