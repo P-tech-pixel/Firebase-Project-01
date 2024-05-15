56f22842d0bbb422e507fb49e42e2b0a7d878d65
Prashanta Pandit <prashanta927pandit@gmail.com>
2024-05-15 15:46:30 +1000

    commit after a Validation of the form, send the request only if the 2 password matches in the registration form.

---
+++ src/JS Scripts/formValidation.js
@@ -0,0 +1,13 @@
+// Function to validate form inputs
+function validateForm() {
+    const password = document.getElementById('password').value;
+    const verifyPassword = document.getElementById('verify_password').value;
+
+    if (password !== verifyPassword) {
+        alert('Passwords do not match');
+        return false;
+    }
+
+    return true;
+}
+export { validateForm };
--- src/JS Scripts/registration.js
+++ src/JS Scripts/registration.js
@@ -1,44 +1,45 @@
-import { auth, fireStoreCollectionReference } from './initializeFirebase.js'; // importing these items from the initializeFirebase.js file.
+import { auth, fireStoreCollectionReference } from './initializeFirebase.js';
 import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
 import { addDoc } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js';
-
 import { spinToLoad } from './customdesign.js';
+import { validateForm } from './formValidation.js';
 
 const registrationForm = document.getElementById('register_form');
 
-registrationForm.addEventListener( 'submit', async function(event) {
-       event.preventDefault();
-       
-       const firstName = registrationForm.firstName.value;
-       const lastName = registrationForm.lastName.value;
-       const email = registrationForm.email.value; 
-       const password = registrationForm.password.value;
-       
-       // create a account using email and password. Firebase authentication
-       createUserWithEmailAndPassword(auth, email, password)
-       .then((userCredential) => {
-         // Signed up successfully
-         var user = userCredential.user;
-         //console.log("User signed up:", user);
-         // Redirect to another page
-         window.location.href = "dashboard.html";
-       })
-       .catch((error) => {
-         // Handle sign-up errors
-         var errorCode = error.code;
-         var errorMessage = error.message;
-         console.error("Sign up error:", errorMessage);
-       });
-
-
-       // Adding the user's input to firestore
+registrationForm.addEventListener('submit', async function(event) {
+    event.preventDefault();
+    
+    // Form validation check; if the validation true ? 
+    if (validateForm()) {
+      const firstName = registrationForm.firstName.value;
+      const lastName = registrationForm.lastName.value;
+      const email = registrationForm.email.value; 
+      const password = registrationForm.password.value;
+      
+      // create an account using email and password with Firebase authentication
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+          // Signed up successfully
+          var user = userCredential.user;
+          // Redirect to another page
+          window.location.href = "dashboard.html";
+      })
+      .catch((error) => {
+          // Handle sign-up errors
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.error("Sign up error:", errorMessage);
+      });
+  
+      // Adding the user's input to firestore
       addDoc(fireStoreCollectionReference, {
-        email: email,   //       const firstName = registrationForm.firstName.value
-        firstName: firstName,  // const lastName = registrationForm.lastName.value
-        lastName: lastName  //     const password = registrationForm.password.value
+          email: email,
+          firstName: firstName,
+          lastName: lastName
       })
-      .then(()=>{
-        spinToLoad();  //invoking the function spinToload() to make a spinning illusion while submitting the form.  
-      })
-
+      .then(() => {
+          spinToLoad(); // Invoking the function spinToLoad() to create a spinning illusion while submitting the form.  
+      });
+    }
 });
+
