4ab48f74e63f8e44a6906ac6a7e5d93ada3adb44
Prashanta Pandit <prashanta927pandit@gmail.com>
2024-05-20 17:20:18 +1000

    working portal, but need to transfer the user email from login to Portal.

--- src/JS_Scripts/firestoreDbUsers.js
+++ src/JS_Scripts/firestoreDbUsers.js
@@ -2,35 +2,77 @@
 import { fireStoreCollectionReference } from './initializeFirebase.js'; // Importing the Firestore collection reference.
 import { onSnapshot, query, where } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js'; // Importing Firestore functions from the Firebase SDK.
 
+// get user firstname using userInputEmail in firebase.
 
-function getUsersEmailsFromFireStoreDb(userEmail) {
-    // Creating a Firestore query to find documents where the email field matches the provided userEmail.
-    const q = query(fireStoreCollectionReference, where("email", "==", userEmail));
+function getUsersFirstName(userInputEmail) {
+  return new Promise((resolve, reject) => {
+    // Creating a Firestore query to find documents where the email field matches the provided userInputEmail.
+    const q = query(fireStoreCollectionReference, where("email", "==", userInputEmail));
 
-      // Setting up a real-time listener to fetch data from Firestore based on the query.
-      onSnapshot(q, (snapshot) => {
-        // if the snapshot is empty.
-        if(snapshot.empty){
-          alert('Your email does not match the database.')
-        }else{
-            let users = []; // Initialize an empty array to hold user details.
+    // Setting up a real-time listener to fetch data from Firestore based on the query.
+    onSnapshot(q, (snapshot) => {
+      let users = []; // Initialize an empty array to hold user details.
 
-            // Iterate through each document in the snapshot and push user data to the users array.
-            snapshot.docs.forEach((doc) => {
-                users.push({ ...doc.data(), id: doc.id });
-            });
- 
-            // Extract email addresses from the users array.
-            let emails = users.map(user => user.email);
- 
-            // Log the extracted email addresses to the console.
-            console.log('Users Emails:', emails);
-        }
+      // Iterate through each document in the snapshot and push user data to the users array.
+      snapshot.docs.forEach((doc) => {
+        users.push({ ...doc.data(), id: doc.id });
+      });
+      
+      // Extract user first name from the users array.
+      let firstNameArray = users.map(user => user.firstName);
+      
+      // Log the extracted first name to the console.
+      let userFirstName = firstNameArray[0];
+
+      resolve(userFirstName);
+    }, (error) => {
+      reject(error);
     });
+  });
 }
 
-// add more functions if you want to get the other attributes of users from firestore database:
+// get user last name using userInputEmail firebase.
 
-export{ getUsersEmailsFromFireStoreDb }; // export this to passwordReset.js and signIn.js to determine the email exist or not in database, while the user input the email before submitting. 
+function getUsersLastName(userInputEmail) {
+  return new Promise((resolve, reject) => {
+    const q = query(fireStoreCollectionReference, where("email", "==", userInputEmail));
+    onSnapshot(q, (snapshot) => {
+      let users = []; 
+      snapshot.docs.forEach((doc) => {
+        users.push({ ...doc.data(), id: doc.id });
+      });
 
+      let lastNameArray = users.map(user => user.lastName);
 
+      let userLastName = lastNameArray[0];
+
+      resolve(userLastName);
+    }, (error) => {
+      reject(error);
+    });
+  });
+}
+
+// get user email using userInputEmail in firebase.
+
+function getUsersEmail(userInputEmail) {
+  return new Promise((resolve, reject) => {
+    const q = query(fireStoreCollectionReference, where("email", "==", userInputEmail));
+    onSnapshot(q, (snapshot) => {
+      let users = []; 
+      snapshot.docs.forEach((doc) => {
+        users.push({ ...doc.data(), id: doc.id });
+      });
+
+      let emailArray = users.map(user => user.email);
+
+      let userEmail = emailArray[0];
+
+      resolve(userEmail);
+    }, (error) => {
+      reject(error);
+    });
+  });
+}
+
+export{ getUsersFirstName, getUsersLastName, getUsersEmail }; 
--- src/JS_Scripts/passwordReset.js
+++ src/JS_Scripts/passwordReset.js
@@ -1,26 +1,24 @@
 import { auth } from './initializeFirebase.js';
-import { getUsersEmailsFromFireStoreDb } from './firestoreDbUsers.js';
 import { sendPasswordResetEmail } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
 
 const passwordResetForm = document.getElementById('passwordResetForm');
 
-passwordResetForm.addEventListener('submit', (event)=>{
+passwordResetForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     var verify_email = passwordResetForm.verify_email.value;
-    
-    //verify if the user exists on fireStore database or not
-    getUsersEmailsFromFireStoreDb(verify_email);
-    
-    //send a password reset request to user
+    const resetRequestMessage = document.getElementById('reset-request-send-message');
+
+    // Attempt to send the password reset email
     sendPasswordResetEmail(auth, verify_email)
-    .then(() => {
-        alert('Password reset request send. Please check your email.');
-    })
-    .catch((error) => {
-       const errorCode = error.code;
-       const errorMessage = error.message;
-       console.error('Error:', errorCode, errorMessage);
-     })
+        .then(() => {
+            resetRequestMessage.innerHTML = `<label>Password reset email sent. Please check your inbox.</label>`;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 });
+
+//NOTE: You need to make sure that the system send a error message when a user input incorect email address.
+
 
